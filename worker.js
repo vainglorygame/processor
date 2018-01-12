d339e1b76c878e83e8741e8e09661c3db7f34b8d
schneefux <schneefux+commit@schneefux.xyz>
2018-01-12 20:23:33 +0100

    add winston file logging

--- worker.js
+++ worker.js
@@ -36,6 +36,10 @@ const logger = new (winston.Logger)({
         new (winston.transports.Console)({
             timestamp: true,
             colorize: true
+        }),
+        new (winston.transports.File)({
+            label: QUEUE,
+            filename: "processor.log"
         })
     ]
 });
